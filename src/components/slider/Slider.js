7c1b5b4a8ff54bb83992f7e2691532bb1618c75e
Adriel Fernandes <adrielfernandes321@gmail.com>
2019-11-28 16:58:26 -0300

    -refactored containers to hooks api -adjusted carousel style

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { withStyles } from '@material-ui/styles'
 import styles from './styles'
 import { Typography } from '@material-ui/core'
@@ -6,19 +6,12 @@ import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
 import ArrowForwardIos from '@material-ui/icons/ArrowForwardIos'
 import PropTypes from 'prop-types'
 
-class Slider extends Component {
-  constructor (props) {
-    super(props)
-    console.log(props.items)
+function Slider(props) {
+    const { classes } = props
+    const [currentItem, setCurrentItem] = useState(0)
+    const [items, setItems] = useState(props.items)
 
-    this.state = {
-      currentItem: 0,
-      items: this.props.items
-    }
-  }
-
-  handleClick = nextItem => {
-    const { currentItem, items } = this.state
+  const handleClick = nextItem => {
 
     const firstItem = 0
     const lastItem = items.length - 1
@@ -26,60 +19,52 @@ class Slider extends Component {
     const isFirstItem = currentItem === firstItem
 
     if (nextItem === 'foward') {
-      this.setState({
-        currentItem: isLastItem ? firstItem : currentItem + 1
-      })
+      setCurrentItem(isLastItem ? firstItem : currentItem + 1)
 
       return
     }
 
-    this.setState({
-      currentItem: isFirstItem ? lastItem : currentItem - 1
-    })
+    setCurrentItem(isFirstItem ? lastItem : currentItem - 1)
   }
 
-  getActiveClass = index => {
-    const { classes } = this.props
-    return index === this.state.currentItem ? classes.itemActive : ''
+  const getActiveClass = index => {
+    return index === currentItem ? classes.itemActive : ''
   }
 
-  getItemDescription = () => this.state.items[this.state.currentItem].title
+  const getItemDescription = () => items[currentItem].title
 
-  renderItems = () => {
-    const { classes } = this.props
-    return this.state.items.map((item, index) => (
+  const renderItems = () => {
+
+    return items.map((item, index) => (
       <div
         key={index}
-        className={`${classes.itemWrapper} ${this.getActiveClass(index)}`}
+        className={`${classes.itemWrapper} ${getActiveClass(index)}`}
       >
         <img src={item.src} alt='' />
         <div className={classes.itemNameWrapper}>
-          <Typography>{this.getItemDescription()}</Typography>
+          <Typography>{getItemDescription()}</Typography>
         </div>
       </div>
     ))
   }
 
-  render () {
-    const { classes } = this.props
-    return (
-      <div className={classes.carouselWrapper}>
-        <span
-          className={classes.carouselBackArrow}
-          onClick={() => this.handleClick('back')}
-        >
-          <ArrowBackIosIcon />
-        </span>
-        {this.renderItems()}
-        <span
-          className={classes.carouselFowardArrow}
-          onClick={() => this.handleClick('foward')}
-        >
-          <ArrowForwardIos />
-        </span>
-      </div>
-    )
-  }
+  return (
+    <div className={classes.carouselWrapper}>
+      <span
+        className={classes.carouselBackArrow}
+        onClick={() => handleClick('back')}
+      >
+        <ArrowBackIosIcon />
+      </span>
+      {renderItems()}
+      <span
+        className={classes.carouselFowardArrow}
+        onClick={() => handleClick('foward')}
+      >
+        <ArrowForwardIos />
+      </span>
+    </div>
+  )
 }
 
 // Slider.propTypes = {
